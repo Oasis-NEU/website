9fe7d57257c8c55821b485b016755fdf44cb1dd2
fraander <57777918+fraander@users.noreply.github.com>
2024-03-01 11:43:59 -0500

    projects redirect

--- src/app/projects/route.ts
+++ src/app/projects/route.ts
@@ -1,7 +1,9 @@
 import { redirect } from "next/navigation";
 
-export default function ProjectsRedirect( ) {
-   redirect(
-     "https://oasisneu.notion.site/f9c8a070d398482ba7a45e42c7982e6a?v=309fabb92ec54fe8bdfcc37523d0c6da&pvs=4"
-   );
+export const runtime = "edge";
+
+export async function GET() {
+  redirect(
+    "https://oasisneu.notion.site/f9c8a070d398482ba7a45e42c7982e6a?v=309fabb92ec54fe8bdfcc37523d0c6da&pvs=4"
+  );
 }
