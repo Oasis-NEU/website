f4adbac973d14dc46cb2f6838070c0f0c437dacc
Laith Taher <lotaher04@gmail.com>
2024-12-04 12:26:47 -0500

    open mentor applciations to the public

--- src/components/MentorAppsBanner.tsx
+++ src/components/MentorAppsBanner.tsx
@@ -18,8 +18,8 @@ export default function MentorAppsBanner({
   ];
   return (
     <TimeGate
-      openDate={new Date("November 24 2024 00:00")}
-      closeDate={new Date("December 1 2024 23:59")}
+      openDate={new Date("December 4 2024 00:00")}
+      closeDate={new Date("December 10 2024 23:59")}
       // waiting={
       //   <BannerCard title={"Eboard applications are opening soon"} buttons={[]}>
       //     {" "}
@@ -28,15 +28,15 @@ export default function MentorAppsBanner({
       // }
     >
       <BannerCard
-        title={"Mentor Applications are OPEN!"}
+        title={"Mentor Applications are back OPEN!"}
         buttons={hideLearnMore ? buttons.slice(0, 1) : buttons.slice(0, 2)}
       >
         <>
           <p>
             Have prior web development experience? Mentor Applications are open
-            for Spring 2025. Apply now! Priority applications close at{" "}
+            for Spring 2025. Apply now! Applications close at{" "}
             <b className="text-oa-blue italic">
-              midnight, Sunday, December 1st
+              midnight, Tuesday, December 10th
             </b>
             .
           </p>
