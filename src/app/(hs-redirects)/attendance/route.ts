4a3331a9cfa6598fac194fda219f87f65445303d
Laith Taher <lotaher04@gmail.com>
2025-01-09 15:33:40 -0800

    updated attendance link for info session

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/vMfg9LmDXa8rmwj78");
+    redirect("https://forms.gle/HMwqg2w1pVLjTnyp7");
 }
