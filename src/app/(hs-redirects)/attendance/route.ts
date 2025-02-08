c3ef8156f53f476a8dc92a4fc3107b59acb2a6b4
CiaranFlanagan <157981950+CiaranFlanagan@users.noreply.github.com>
2025-02-08 11:24:06 -0500

    updated attendence link

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/TXRJT2hiuiCwzwH6A");
+  redirect("https://forms.gle/TvacC7eRWtuS42zk9");
 }
