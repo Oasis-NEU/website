fcad64391587d6f7eb016f28ffaa2a25fa977c7e
Laith Taher <lotaher04@gmail.com>
2025-02-09 11:20:44 -0500

    fixed attendance

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,7 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/TvacC7eRWtuS42zk9");
+  redirect(
+    "https://docs.google.com/forms/d/e/1FAIpQLSc8iPmO3bJ_i2Pd5gE5vuv-vN1HlJ3NA9vpGyxUnOCnLUgbWA/viewform?usp=sharing"
+  );
 }
