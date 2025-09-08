3f18b2fea138fdb4613d23896fd749c87ce88136
Laith Taher <lotaher04@gmail.com>
2025-09-08 09:44:40 -0400

    updated attendance

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/JPrjiDEuoCY1Rdnc7");
+  redirect("https://forms.gle/k4JmeJS3q3s6SAJH9");
 }
