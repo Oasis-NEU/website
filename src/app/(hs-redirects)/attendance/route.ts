e621ffaa4ae46fe63aa96830649597bd1cc55990
Ciaran Flanagan <flanagan.ci@northeastern.edu>
2025-11-09 11:56:24 -0500

    attendance update

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -4,6 +4,6 @@ export const runtime = "edge";
 
 export async function GET() {
   redirect(
-    "https://docs.google.com/forms/d/e/1FAIpQLSddD295Wr4pvrqMMB6WcukOpBFXPVo3egTGvtwBi3R3U8CkJg/viewform?usp=header"
+    "https://docs.google.com/forms/d/e/1FAIpQLSfbmQpSNGahjH6S0oi298PxC6zGtjOnGlj43Ru24qXQricAzA/viewform?usp=header"
   );
 }
