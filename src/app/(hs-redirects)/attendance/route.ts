5185130b057abe4397355730a0b225d908323be8
Ashley Zheng <zheng.as@northeastern.edu>
2026-01-25 11:06:26 -0500

    attendance update

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/dQ1tSFcFBqECAQ2i9");
+  redirect("https://forms.gle/LEinKf6rxs3D6ZA99");
 }
