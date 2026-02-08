c314327d98685ae0e9c718fe1a5c871125f4010d
Ciarán Flanagan <ciaranflanagan@Ciarans-MacBook-Pro.local>
2026-02-08 12:04:03 -0500

    hs3 attendance link

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,11 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
+<<<<<<< HEAD
   redirect("https://forms.gle/NWzk3ULkX2U3P2bv7");
+||||||| parent of 852291c9 (hs3 attendance link)
+  redirect("https://forms.gle/LEinKf6rxs3D6ZA99");
+=======
+  redirect("https://forms.gle/frQTLtuWiZ5GXwLA8");
+>>>>>>> 852291c9 (hs3 attendance link)
 }
