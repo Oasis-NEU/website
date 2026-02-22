bdecc6b9cb30e3b1a153fe734ff8cc17b35a5960
Ciarán Flanagan <ciaranflanagan@Ciarans-MacBook-Pro.local>
2026-02-22 12:00:47 -0500

    Hs4 attendance

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/4HKjjcVkqN6iH58z9");
+  redirect("https://forms.gle/QAL3J6sMvgMmstsX9");
 }
