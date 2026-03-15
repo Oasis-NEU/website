b798d9010a3374880234743a05e6ad470ab35a82
Ciarán Flanagan <ciaranflanagan@ciarans-mbp.myfiosgateway.com>
2026-03-15 11:43:14 -0400

    HS6 attendance

--- src/app/(hs-redirects)/attendance/route.ts
+++ src/app/(hs-redirects)/attendance/route.ts
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const runtime = "edge";
 
 export async function GET() {
-  redirect("https://forms.gle/QAL3J6sMvgMmstsX9");
+  redirect("https://forms.gle/2QzsrxGF1n4Pw1bU6");
 }
